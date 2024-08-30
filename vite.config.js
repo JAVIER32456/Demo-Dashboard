c14ae09309fb49eef823ecee8e9f5952f613c50e
JAVIER32456 <ronaltomas932@gmail.com>
2024-08-30 12:08:04 -0500

    nuevo link

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://JAVIER32456.github.io/Demo-Dashboard",
+  base:"https://JAVIER32456.github.io/demo-dashboard",
 })
